b6dcf124c412eb187bd8d44fd697af4a6947b3eb
Mirko <mskroch@uniglow-entertainment.com>
2021-01-25 17:31:53 +0100

    Imported scripting packages

---
+++ Assets/Plugins/UnityGlowFramework/Utility/Constants.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UniGlow
+{
+	public class Constants
+	{
+        #region Tags and Layers
+        public static readonly string TAG_PLAYER = "Player";
+        #endregion
+
+        #region Scenes
+
+        #endregion
+
+        #region Sounds
+        // Exposed Parameters in Mixers
+        public static readonly string MIXER_SFX_VOLUME = "SFXVolume";
+        public static readonly string MIXER_MUSIC_VOLUME = "MusicVolume";
+        #endregion
+    }
+}
---
+++ Assets/Plugins/UnityGlowFramework/Utility/GameEvents.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UniGlow.Utility
+{
+	public class GameEvents
+	{
+        public delegate void ScanningHandler(Vector3 trackedPosition, Quaternion trackedRotation);
+        public static event ScanningHandler ScanningCompleted;
+
+        public static void CompleteScan(Vector3 trackedPosition, Quaternion trackedRotation)
+        {
+            ScanningCompleted?.Invoke(trackedPosition, trackedRotation);
+        }
+
+        public delegate void ScanStartHandler();
+        public static event ScanStartHandler ScanningStarted;
+
+        public static void StartScan()
+        {
+            ScanningStarted?.Invoke();
+        }
+
+        public delegate void POITapHandler(GameObject poiTapped);
+        public static event POITapHandler POITapped;
+
+        public static void TapPOI(GameObject poiTapped)
+        {
+            POITapped?.Invoke(poiTapped);
+        }
+
+        public delegate void TimeChangeHandler(int newTimeDomain);
+        public static event TimeChangeHandler TimeDomainChanged;
+
+        public static void ChangeTimeDomain(int newTimeDomain)
+        {
+            TimeDomainChanged?.Invoke(newTimeDomain);
+        }
+    }
+}
